1cf0c715a14f5b3907145d94c1a62dd9a5886d4c
Aniket Umredkar <aniketumredkar@plobalapps.com>
2023-10-28 23:20:25 +0530

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,50 @@
 const User = require("../models/user");
 
-exports.createUser = (req, res) => {
-    console.log(req.body);
-    const user = new User(req.body.email, req.body.password, req.first_name, req.last_name);
-    user.save()
+exports.signInController = (req, res) => {
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const email = req.body.email;
+    const password = req.body.password;
+    User.create({
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password,
+    })
         .then(() => {
             res.status(200).send({ message: "successfull" });
         })
-        .catch((error) => {
-            res.status(400).send({ message: error });
+        .catch((err) => {
+            console.log(err);
+            res.send(400).send({ message: "error" });
         });
 };
 
-exports.authenticateUser = (req, res) => {
+exports.loginController = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findAll({
+        attributes: ["first_name", "last_name"],
+        where: {
+            email: email,
+            password: password,
+        },
+    })
+        .then((user) => {
+            res.status(200).send(user[0]);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).send({ message: "error" });
+        });
+};
+
+exports.forgotPasswordController = (req, res) => {
     console.log(req.body);
-    const user = new User(req.body.email, req.body.password);
     res.status(200).send({ message: "successfull" });
 };
 
-exports.User = (req, res) => {
+exports.resetPasswordController = (req, res) => {
     console.log(req.body);
-    const user = new User(req.body.email, req.body.password);
     res.status(200).send({ message: "successfull" });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,56 +1,19 @@
 const express = require("express");
-const User = require("../models/user");
-const sequelize = require("../utils/database");
+const {
+    signInController,
+    loginController,
+    forgotPasswordController,
+    resetPasswordController,
+} = require("../controllers/auth");
 
 const router = express.Router();
 
-router.post("/sign-in", (req, res) => {
-    const first_name = req.body.first_name;
-    const last_name = req.body.last_name;
-    const email = req.body.email;
-    const password = req.body.password;
-    User.create({
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        password: password,
-    })
-        .then(() => {
-            res.status(200).send({ message: "successfull" });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.send(400).send({ message: "error" });
-        });
-});
+router.post("/sign-in", signInController);
 
-router.post("/login", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    User.findAll({
-        attributes: ['first_name', 'last_name'],
-        where: {
-            email: email,
-            password: password,
-        },
-    })
-        .then((user) => {
-            res.status(200).send(user[0]);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(400).send({ message: "error" });
-        });
-});
+router.post("/login", loginController);
 
-router.post("/forgot-password", (req, res) => {
-    console.log(req.body);
-    res.status(200).send({ message: "successfull" });
-});
+router.post("/forgot-password", forgotPasswordController);
 
-router.post("/reset-password", (req, res) => {
-    console.log(req.body);
-    res.status(200).send({ message: "successfull" });
-});
+router.post("/reset-password", resetPasswordController);
 
 module.exports = router;
