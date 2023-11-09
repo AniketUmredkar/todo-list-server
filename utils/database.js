a9bc5ea70d4f4e1e42eb872313184e7ecc128763
Aniket Umredkar <aniketumredkar@plobalapps.com>
2023-11-09 16:09:39 +0530

    commit

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
 
+console.log("user", process.env.DATABASE_USER);
+
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     dialect: "mysql",
     host: process.env.DATABASE_HOST,
